f5f2c6170519c7a5ef04c8a02661c94f036cc489
Shubham Kumar Mahato <samshubham121@gmail.com>
2025-10-04 18:58:28 +0530

    Feat:Expense model added

---
+++ src/main/java/com/shubham/intern/expense_tracker/model/Expense.java
@@ -0,0 +1,41 @@
+package com.shubham.intern.expense_tracker.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Amount Cannot be null")
+    @Positive(message = "Amount must be Positive")
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @NotNull(message = "Data cannot be null")
+    @Column(nullable = false)
+    @PastOrPresent(message = "Data cannot be in future")
+    private String data;
+
+    @Size(max=255 , message = "Note cannot be longer than 255 Characters" )
+    private String note;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "category_id", nullable = false)
+    @NotNull(message = "Category cannot be null")
+    private Category category;
+}
---
+++ src/main/java/com/shubham/intern/expense_tracker/repository/ExpenseRepository.java
@@ -0,0 +1,4 @@
+package com.shubham.intern.expense_tracker.repository;
+
+public interface ExpenseRepository {
+}
