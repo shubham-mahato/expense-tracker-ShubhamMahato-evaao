e9172eec7c6ff2e5c32b9611210c0531f3c952e3
Shubham Kumar Mahato <samshubham121@gmail.com>
2025-10-04 20:24:42 +0530

    Feat:REST Controller for expense API

---
+++ src/main/java/com/shubham/intern/expense_tracker/controller/ExpenseController.java
@@ -0,0 +1,51 @@
+package com.shubham.intern.expense_tracker.controller;
+
+
+import com.shubham.intern.expense_tracker.model.Expense;
+import com.shubham.intern.expense_tracker.service.ExpenseService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/expenses")
+public class ExpenseController {
+
+    private final ExpenseService expenseService;
+
+    public ExpenseController(ExpenseService expenseService) {
+        this.expenseService = expenseService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Expense> addExpense(@Valid @RequestBody Expense expense) {
+        Expense newExpense = expenseService.addExpense(expense);
+        return new  ResponseEntity<>(newExpense, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<Expense> getAllExpenses() {
+        return expenseService.getAllExpense();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Expense> getExpenseById(@PathVariable Long id) {
+        Expense expense = expenseService.getExpenseById(id);
+        return new ResponseEntity<>(expense, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Expense>  updateExpense(@PathVariable Long id, @Valid @RequestBody Expense expenseDetails) {
+        Expense updatedExpense = expenseService.updateExpense(id, expenseDetails);
+        return ResponseEntity.ok(updatedExpense);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteExpenseById(@PathVariable Long id) {
+        expenseService.deleteExpense(id);
+        return ResponseEntity.noContent().build();
+    }
+}
