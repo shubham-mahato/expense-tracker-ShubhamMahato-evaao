718ff6161f479b734d2a43f4b6a4b8f376df1e50
Shubham Kumar Mahato <samshubham121@gmail.com>
2025-10-04 19:02:24 +0530

    feat:Added Expense Repo

--- src/main/java/com/shubham/intern/expense_tracker/repository/ExpenseRepository.java
+++ src/main/java/com/shubham/intern/expense_tracker/repository/ExpenseRepository.java
@@ -1,4 +1,15 @@
 package com.shubham.intern.expense_tracker.repository;
 
-public interface ExpenseRepository {
+import com.shubham.intern.expense_tracker.model.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+
+    List<Expense> findByCategory_Name(String categoryName);
+
+    List<Expense> findByDateBetween(LocalDate startDate, LocalDate endDate);
+
 }
