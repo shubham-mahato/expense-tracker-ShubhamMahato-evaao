839bdb5990c083fa6fac197a63607daf0ce6f8b4
Shubham Kumar Mahato <samshubham121@gmail.com>
2025-10-04 20:08:24 +0530

    Feat:Added Business Logic in ExpenseService

---
+++ src/main/java/com/shubham/intern/expense_tracker/service/ExpenseService.java
@@ -0,0 +1,60 @@
+package com.shubham.intern.expense_tracker.service;
+
+import com.shubham.intern.expense_tracker.exception.ResourceNotFoundException;
+import com.shubham.intern.expense_tracker.model.Category;
+import com.shubham.intern.expense_tracker.model.Expense;
+import com.shubham.intern.expense_tracker.repository.CategoryRepository;
+import com.shubham.intern.expense_tracker.repository.ExpenseRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ExpenseService {
+
+    private final ExpenseRepository expenseRepository;
+    private final CategoryRepository categoryRepository;
+
+    public  ExpenseService(ExpenseRepository expenseRepository, CategoryRepository categoryRepository) {
+        this.expenseRepository = expenseRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Expense> getAllExpense() {
+        return expenseRepository.findAll();
+    }
+
+    public Expense getExpenseById(Long id) {
+        return expenseRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Expense not Found with id: "+ id));
+    }
+
+    @Transactional
+    public Expense addExpense(Expense expense) {
+        Category category = categoryRepository.findByName(expense.getCategory().getName())
+                .orElseGet(() -> categoryRepository.save(expense.getCategory()));
+        expense.setCategory(category);
+        return expenseRepository.save(expense);
+    }
+
+    @Transactional
+    public Expense updateExpense(Long id, Expense expenseDetails) {
+        Expense existingExpense = getExpenseById(id);
+
+        Category category = categoryRepository.findByName(expenseDetails.getCategory().getName())
+                .orElseGet(() -> categoryRepository.save(expenseDetails.getCategory()));
+
+        existingExpense.setAmount(expenseDetails.getAmount());
+        existingExpense.setData(expenseDetails.getData());
+        existingExpense.setNote(expenseDetails.getNote());
+        existingExpense.setCategory(category);
+        return expenseRepository.save(existingExpense);
+    }
+
+    public void deleteExpense(Long id) {
+        if(!expenseRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Expense not Found with id: "+ id);
+        }
+        expenseRepository.deleteById(id);
+    }
+}
